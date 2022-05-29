a7987830412fd45afd3bdf6799509077c8342ea3
ivteplo <iteplov7@gmail.com>
2022-05-29 17:59:34 +0300

    Initialize Go module

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ivteplo/home-gallery
+
+go 1.18
---
+++ server/main.go
@@ -0,0 +1,9 @@
+// Copyright (c) 2022 Ivan Teplov
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+}
