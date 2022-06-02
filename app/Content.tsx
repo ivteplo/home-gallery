b7c7a7922343a76cddf40106f2b2e4507e779963
ivteplo <iteplov7@gmail.com>
2022-06-02 13:59:43 +0300

    Move icons into assets folder

--- app/Content.tsx
+++ app/Content.tsx
@@ -11,10 +11,10 @@ const Upload = lazy(() => import("./pages/Upload"))
 const Albums = lazy(() => import("./pages/Albums"))
 const Settings = lazy(() => import("./pages/Settings"))
 
-import GalleryIcon from "./icons/GalleryIcon"
-import UploadIcon from "./icons/UploadIcon"
-import AlbumsIcon from "./icons/AlbumsIcon"
-import SettingsIcon from "./icons/SettingsIcon"
+import GalleryIcon from "./assets/icons/GalleryIcon"
+import UploadIcon from "./assets/icons/UploadIcon"
+import AlbumsIcon from "./assets/icons/AlbumsIcon"
+import SettingsIcon from "./assets/icons/SettingsIcon"
 
 const Content: Component = () => {
   const [t, _] = useI18n()
