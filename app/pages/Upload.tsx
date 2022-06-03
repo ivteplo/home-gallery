16febb9ae4d111184482d35c13fb402a47658f0b
ivteplo <iteplov7@gmail.com>
2022-06-03 19:56:59 +0300

    Update page for uploading media

--- app/pages/Upload.tsx
+++ app/pages/Upload.tsx
@@ -2,12 +2,29 @@
 
 import { useI18n } from "@solid-primitives/i18n"
 import { Component } from "solid-js"
+
+import Button from "../components/Button"
 import Page from "../components/Page"
+import Section from "../components/Section"
+import Text from "../components/Text"
+
+import UploadIcon from "../assets/icons/UploadIcon"
 
 const Upload: Component = () => {
   const [t, _] = useI18n()
 
-  return <Page pageTitle={t("upload")}></Page>
+  return (
+    <Page pageTitle={t("upload")}>
+      <Section class="fill text-center items-center">
+        <Text textStyle="paragraph">{t("uploadPageText")}</Text>
+        <Button
+          buttonStyle="primary"
+          icon={<UploadIcon />}
+          label={t("uploadMedia")}
+        />
+      </Section>
+    </Page>
+  )
 }
 
 export default Upload
