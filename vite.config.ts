2d3ffe104fa1b49467d1e8961d70deaef118e2d6
ivteplo <iteplov7@gmail.com>
2022-06-08 21:57:11 +0300

    Setup proxy for development front-end app

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
 import { defineConfig } from "vite"
 import solidPlugin from "vite-plugin-solid"
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   root: "./app",
   plugins: [solidPlugin()],
+  server:
+    mode !== "development"
+      ? {}
+      : {
+          proxy: {
+            "/api": "http://localhost:8080"
+          },
+        },
   build: {
     target: "esnext",
     polyfillDynamicImport: false,
   },
-})
+}))
