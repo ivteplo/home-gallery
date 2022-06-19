e78bd9bcb859643c12e1647cd34e05650b612c39
ivteplo <iteplov7@gmail.com>
2022-06-19 20:19:40 +0300

    Try to fix problem with the testing workflow

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -23,7 +23,7 @@ func main() {
 
     config := api.CreateConfig()
 
-    err := helpers.ParseJSONFile(configPath, &config)
+    err := helpers.ParseJSONFile(configPath, config)
     if err != nil {
         log.Fatal(err)
     }
--- internal/helpers/json.go
+++ internal/helpers/json.go
@@ -3,31 +3,31 @@
 package helpers
 
 import (
-	"encoding/json"
-	"io/ioutil"
-	"log"
-	"net/http"
-	"os"
+    "encoding/json"
+    "io/ioutil"
+    "log"
+    "net/http"
+    "os"
 )
 
 func ParseJSONFile[OutputStruct any](path string, output *OutputStruct) error {
-	jsonFile, err := os.Open(path)
-	if err != nil {
-		return err
-	}
+    jsonFile, err := os.Open(path)
+    if err != nil {
+        return err
+    }
 
-	defer jsonFile.Close()
+    defer jsonFile.Close()
 
-	byteValue, _ := ioutil.ReadAll(jsonFile)
-	json.Unmarshal(byteValue, output)
+    byteValue, _ := ioutil.ReadAll(jsonFile)
+    json.Unmarshal(byteValue, output)
 
-	return nil
+    return nil
 }
 
-func OutputJSONTo[Struct any](writer http.ResponseWriter, object Struct) {
-	writer.Header().Set("Content-Type", "application/json")
+func OutputJSONTo(writer http.ResponseWriter, object any) {
+    writer.Header().Set("Content-Type", "application/json")
 
-	if err := json.NewEncoder(writer).Encode(object); err != nil {
-		log.Fatal(err)
-	}
+    if err := json.NewEncoder(writer).Encode(object); err != nil {
+        log.Fatal(err)
+    }
 }
